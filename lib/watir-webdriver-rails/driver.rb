c96c2a8c56927e4ee3acf168e18a02dae4f0bdb1
malixsys <github@malix.com>
2012-01-31 17:20:06 -0500

    added start-minimized

--- lib/watir-webdriver-rails/driver.rb
+++ lib/watir-webdriver-rails/driver.rb
@@ -14,7 +14,7 @@ module WatirWebdriverRails
         when :chrome
           profile = Selenium::WebDriver::Chrome::Profile.new
           profile['download.prompt_for_download'] = false
-          switches = %w[--ignore-certificate-errors --disable-popup-blocking --disable-translate]
+          switches = %w[--ignore-certificate-errors --disable-popup-blocking --disable-translate --start-minimized]
           Selenium::WebDriver.for :chrome, :profile => profile, :switches => switches
         else
           raise "Invalid dirver: " + @driver.to_s
